ec6a07046704b5cb74d2f401ad97851ce93e8ed3
Andrew Caya <andrewscaya@yahoo.ca>
2023-03-03 17:27:12 -0500

    2023-03-03 AC: Updates the routes, controllers, and some middleware.

--- config/routes.config.php
+++ config/routes.config.php
@@ -6,24 +6,9 @@ $baseConfig['routes'] = [
         '/',
         'index',
     ],
-    [
+    /*[
         'GET',
         '/index[/{action}]',
         'index',
-    ],
-    [
-        ['GET', 'POST'],
-        '/products[/{action}[/{id:[0-9]+}]]',
-        'products',
-    ],
-    [
-        'GET',
-        '/baz[/{action}]',
-        'specialmodule/index',
-    ],
-    [
-        'GET',
-        '/admin[/{action}]',
-        'specialmodule/index',
-    ],
+    ],*/
 ];
